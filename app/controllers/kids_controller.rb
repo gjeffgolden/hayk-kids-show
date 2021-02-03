85adb19f9bb818842b0bc0c38a4fa5aee7b03689
gjeffgolden <gjgolden08@gmail.com>
2021-02-03 15:58:35 -0700

    Done

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -6,13 +6,20 @@ class KidsController < ApplicationController
   end
 
   def create
-    @kid = Kid.create({
-      username: params[:kid][:username],
-      description: params[:kid][:description],
-      favorite_color: params[:kid][:favorite_color],
-      age: params[:kid][:age],
-    })
+    @kid = Kid.new(kid_params)
 
-    render json: { kid: @kid }
+    if @kid.valid?
+      @kid.save
+      render json: @kid, status: :created
+    else
+      render json: {errors: @kid.errors.full_messages}, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:username, :description, :favorite_color, :age)
+  end
+
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,5 @@
 class Kid < ApplicationRecord
+    validates :age, presence: true, numericality: {greater_than: 6, less_than: 15}
+    validates :username, presence: true, uniqueness: true, length: {minimum: 7, maximum: 25}
+
 end
